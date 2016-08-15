91b1a20494fcf5191e9386e0be0b01e018c870c3
Michael Malleske <mmalleske@gmail.com>
2016-08-15 14:52:12 -0500

    can save lists to savedlist collection

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,43 @@ var router = express.Router();
 var Item = require('../models/item');
 var SavedList = require('../models/saved-list');
 
-//GET
+//GET Current List
 router.get('/', function(req, res, next) {
   Item.find({}, function(err, items){
     if (err) console.log(err);
     res.render('index', { title: 'Pantry', items: items });
   })
 });
+
 //NEW
 router.get('/new', function(req, res, next) {
   //create new item form
   res.render('new', { title: 'Pantry' });
 });
-//SHOW
-router.get('/:id', function(req, res, next) {
-  var id = req.params.id;
-  Item.findOne({_id: id }, function(err, item){
+//GET Saved Lists
+router.get('/saved-lists', function(req, res, next) {
+  SavedList.find({}, function(err, savedLists){
     if (err) console.log(err);
-    res.render('show', {title: 'Item', item: item})
+    res.render('saved-lists', { title: 'Saved Lists', savedLists: savedLists });
   })
 });
+//Get Individual List
+router.get('/saved-lists/:id', function(req, res, next) {
+  var id = req.params.id;
+  SavedList.findOne({_id: id }, function(err, savedList){
+    if (err) console.log(err);
+    res.render('saved-list', { title: 'Saved List', savedList: savedList });
+  })
+});
+
+//SHOW
+// router.get('/:id', function(req, res, next) {
+//   var id = req.params.id;
+//   Item.findOne({_id: id }, function(err, item){
+//     if (err) console.log(err);
+//     res.render('show', {title: 'Item', item: item})
+//   })
+// });
 //EDIT
 router.get('/:id/edit', function(req, res, next) {
   var id = req.params.id;
@@ -31,7 +48,17 @@ router.get('/:id/edit', function(req, res, next) {
     res.render('edit', {title: 'Pantry', item: item})
   })
 });
-
+//CREATE NEW SAVED LIST
+router.post('/saved-lists/', function(req, res, next){
+  // create a item then redirect to index
+  var newSavedList = new SavedList({
+    name: req.body.name
+  });
+  newSavedList.save(function(err, savedList){
+    if (err) console.log(err);
+    res.redirect('/saved-lists');
+  });
+});
 //CREATE
 router.post('/', function(req, res, next){
   // create a item then redirect to index
@@ -44,6 +71,7 @@ router.post('/', function(req, res, next){
     res.redirect('/');
   });
 });
+
 //UPDATE
 router.patch('/:id', function(req, res, next) {
   Item.findByIdAndUpdate(req.params.id, req.body, function(err, item){
@@ -51,7 +79,15 @@ router.patch('/:id', function(req, res, next) {
     res.redirect('/');
   })
 });
-//DELETE
+//DELETE LIST
+router.delete('saved-lists/:id', function(req, res, next){
+  // delete specific item and redirect back to index
+  Item.findByIdAndRemove(req.params.id, req.body, function(err, item){
+    if (err) console.log(err);
+    res.redirect('/saved-lists');
+  })
+});
+//DELETE ITEM
 router.delete('/:id', function(req, res, next){
   // delete specific item and redirect back to index
   Item.findByIdAndRemove(req.params.id, req.body, function(err, item){
@@ -59,6 +95,7 @@ router.delete('/:id', function(req, res, next){
     res.redirect('/');
   })
 });
+
 
 
 module.exports = router;
